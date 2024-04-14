0d6a5cddc3dcbc9152babae620f585f677047f45
Oceanlzj <oceanlzj@outlook.com>
2024-04-14 17:19:12 +0800

    Basic DataModel

---
+++ Assets/BasicModules/Model/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player
+{
+  //
+  public string Name { get; set; }
+
+  //Player statices here ...
+
+  
+  public int Sanity {  get; set; }
+
+
+
+}
