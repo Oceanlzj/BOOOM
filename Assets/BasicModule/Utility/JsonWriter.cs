97824673b24e0b101155a611f9ed349d6986faba
Oceanlzj <oceanlzj@outlook.com>
2024-04-18 15:09:50 +0800

    Added Nuget Package Basic Json Read and write

---
+++ Assets/BasicModule/Utility/JsonWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Assets.BasicModule.Utility
+{
+  public class JsonWriter<T>
+  {
+    public static void Write(T obj, string Path)
+    {
+      using (StreamWriter sw = new StreamWriter(Application.persistentDataPath + '\\' + Path))
+      {
+        sw.WriteLine(JsonSerializer.Serialize(obj, typeof(T)));
+        sw.Close();
+        sw.Dispose();
+      }
+    }
+  }
+}
