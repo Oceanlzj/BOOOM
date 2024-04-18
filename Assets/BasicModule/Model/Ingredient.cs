9927a446a7428bca92f41c620e2a56347156f12f
Oceanlzj <oceanlzj@outlook.com>
2024-04-18 15:09:50 +0800

    Added Nuget Package Basic Json Read and write

--- Assets/BasicModule/Model/Ingredient.cs
+++ Assets/BasicModule/Model/Ingredient.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-
-public class Ingredient
+namespace Assets.BasicModule.Model
 {
-  public enum IngredienType
+  [Serializable]
+  public class Ingredient
   {
-    None = 0,
-    Normal = 1,
-    Special = 2
+    public enum IngredientType
+    {
+      None = 0,
+      Normal = 1,
+      Special = 2
+    }
+
+    public int ID { get; set; }
+    public IngredientType Type { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double IngredientHealth { get; set; }
+    public double IngredientSanity { get; set; }
+    public double IngredientSatiety { get; set; }
+    public FoodProperty IngredientProperty { get; set; }
+
+    public Ingredient() { }
+    public Ingredient(int iD, IngredientType type, string name, string description, double ingredientHealth, double ingredientSanity, double ingredientSatiety, FoodProperty ingredientProperty)
+    {
+      ID = iD;
+      Type = type;
+      Name = name;
+      Description = description;
+      IngredientHealth = ingredientHealth;
+      IngredientSanity = ingredientSanity;
+      IngredientSatiety = ingredientSatiety;
+      IngredientProperty = ingredientProperty;
+    }
   }
-
-  public int ID {  get; set; }
-  public IngredienType Type { get; set; }
-  public string Name { get; set; }
-  public string Description { get; set; }
-
 }
---
+++ Assets/BasicModule/Utility/JsonReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Text.Json;
+
+
+namespace Assets.BasicModule.Utility
+{
+  //read json file to class xml?
+  public class JsonReader<T>
+  {
+
+
+    public static T Read(string FilePath)
+    {
+      string JsonFile = "";
+      using (StreamReader sr = new StreamReader(Application.dataPath + '\\' + FilePath))
+      {
+        JsonFile = sr.ReadToEnd();
+        sr.Close();
+        sr.Dispose();
+      }
+      return JsonSerializer.Deserialize<T>(JsonFile);
+    }
+
+
+
+  }
+}
