f6cf06b69c89263dad9605d10b7fb7c3ffd6826f
Oceanlzj <oceanlzj@outlook.com>
2024-04-22 03:22:29 +0800

    Add Assert and Model refactor

--- Assets/BasicModule/Model/Recipe.cs
+++ Assets/BasicModule/Model/Recipe.cs
@@ -1,18 +1,18 @@
-﻿using Assets.BasicModule.Utility;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
 
 namespace Assets.BasicModule.Model
 {
   [Serializable]
   public class Recipe
   {
-    public int Ingredient_ID1;
-    public int Ingredient_ID2;
-    public int Dish_ID;
+    public int Ingredient_ID1 {get; set;}
+    public int Ingredient_ID2 { get; set;}
+    public int Dish_ID { get; set;}
+    public double Difficulty { get; set;}
   }
 }
--- Assets/Scripts/TestScene/TestMain.cs
+++ Assets/Scripts/TestScene/TestMain.cs
@@ -3,29 +3,17 @@ using Assets.BasicModule.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TestMain : MonoBehaviour
 {
-  // Start is called before the first frame update
+
+
   void Start()
   {
-
-    //Dish d = new Dish(0,"Test", "TestDes", 10.2, 3.3, 20, 1.0, Dish.DishType.Normal,
-    //  new List<FoodProperty>() { FoodProperty.Normal, FoodProperty.Acid});
-
-    //JsonWriter<Dish>.Write(d, "test.json");
-
-    //Dish a = JsonReader<Dish>.Read("Data\\Dish\\test.json");
-
-    //string Path = "Recipe.json";
-    //Recipe SingleRecipe = new Recipe();
-    //SingleRecipe.Recipes.Add("1,2", 1);
-    //JsonWriter<Recipe>.Write(SingleRecipe, Path);
-
-    //Recipe recipe = JsonReader<Recipe>.Read("Data\\Recipe\\Recipe.json");
+    Assets.BasicModule.Factory.DataFactory.Instance().getTaskByID(0);
   }
 
-  // Update is called once per frame
   void Update()
   {
 
