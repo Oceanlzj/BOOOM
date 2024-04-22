8774f510df3ac66dc73de60973278e37445d74d9
carlos <851917050@qq.com>
2024-04-22 20:59:31 +0800

    切割QTE初版

--- Assets/Scripts/QteGame/Pointer.cs
+++ Assets/Scripts/QteGame/Pointer.cs
@@ -10,9 +10,27 @@ public class Pointer : MonoBehaviour
     private bool _inArea = false;
 
     private bool _vector = false;
+
+    private bool _clockwise = true;
+    
+    
+    
+    //private float origionZ;
+    //private Quaternion targetRotation;
+    //public float RotateAngle = 60;
+    //public int count = 0;
+    //private bool i;
+    
+    //private Transform from;
+    //private Transform to;
+//
+    //private float timeCount = 0.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
+        //from.rotation = new Quaternion(0,0,75,0);
+        //to.rotation = new Quaternion(0,0,-150,0);
     }
 
     // Update is called once per frame
@@ -22,6 +40,10 @@ public class Pointer : MonoBehaviour
         {
             _vector = true;
         }
+        
+        //transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount);
+        //timeCount = timeCount + Time.deltaTime;
+        //transform.Rotate();
     }
 
     private void FixedUpdate()
@@ -32,7 +54,34 @@ public class Pointer : MonoBehaviour
         }
         else
         {
-            transform.RotateAround(transform.position, -transform.forward, speed+Time.deltaTime);
+            if (transform.rotation.z > 0.608f)
+            {
+                _clockwise = true;
+            }
+            else if (transform.rotation.z < -0.966f)
+            {
+                _clockwise = false;
+            }
+            
+            Vector3 direction = transform.forward; 
+            if (_clockwise)
+            {
+                direction = -direction;
+            }
+            
+            transform.RotateAround(transform.position, direction, speed+Time.deltaTime);
+            
+            
+            
+            
+            //z轴 75,-150
+            //targetRotation = Quaternion.Euler(0,0,RotateAngle*count+origionZ) * Quaternion.identity;
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2);
+            ////避免误差
+            //if (Quaternion.Angle(targetRotation, transform.rotation) < 1)
+            //    transform.rotation = targetRotation;
+            
+            
         }
     }
 
@@ -41,6 +90,7 @@ public class Pointer : MonoBehaviour
         if (other.tag == "DropPort")
         {
             _inArea = true;
+            print("enter");
         }
     }
 
@@ -49,6 +99,8 @@ public class Pointer : MonoBehaviour
         if (other.tag == "DropPort")
         {
             _inArea = false;
+            print("Exit");
         }
     }
+    
 }
