12bbaf76bc6eeba5e2fbc7ee3a8e166307bc9dad
Oceanlzj <oceanlzj@outlook.com>
2024-04-27 21:09:08 +0800

    Model adjust

--- Assets/BasicModule/Model/Worker.cs
+++ Assets/BasicModule/Model/Worker.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 
 namespace Assets.BasicModule.Model
 {
@@ -27,73 +28,116 @@ namespace Assets.BasicModule.Model
 
     [Category("[0] ID"), Description("唯一ID")]
     public int ID { get; set; }
-    [Category("[6] Type"), Description("员工生物类型")]
-    public WorkerType Type { get; set; }
-    [Category("[2] Description"), Description("员工名")]
+
+    [Category("[1] Description"), Description("员工名")]
     public string Name { get; set; }
-    [Category("[2] Description"), Description("员工描述")]
+    [Category("[1] Description"), Description("员工描述")]
     public string Description { get; set; }
 
     //健康度
-    [Category("[3] Health"), Description("初始健康值"), DisplayName("[0] Health")]
+    [Category("[2] Health"), Description("初始健康值"), DisplayName("[0] Health")]
     public double Health { get; set; }
-    [Category("[3] Health"), Description("健康值上限"), DisplayName("[1] HealthMax")]
+    [Category("[2] Health"), Description("健康值上限"), DisplayName("[1] HealthMax")]
     public double HealthMax { get; set; }
-    [Category("[3] Health"), Description("健康值变化量"), DisplayName("[2] BasicHealthDecrease")]
+    [Category("[2] Health"), Description("健康值变化量"), DisplayName("[2] BasicHealthDecrease")]
     public double BasicHealthDecrease { get; set; }
+    [Category("[2] San"), Description("标准化HP低标准 : ([0, 1])"), DisplayName("[3] NormalizedLowHealthBar")]
+    public double NormalizedLowHealthBar { get; set; }
 
-    [Category("[3] Health"), Description("健康值变化倍率"), DisplayName("[3] HealthMultiplier")]
-    public double HealthMultiplier { get; set; }
+    [Category("[2] Health"), Description("存活")]
+    public bool IsAlive { get; set; } = true;
 
     //饱食度
-    [Category("[4] Satiety"), Description("初始饱食度"), DisplayName("[0] Satiety")]
+    [Category("[3] Satiety"), Description("初始饱食度"), DisplayName("[0] Satiety")]
     public double Satiety { get; set; }
-    [Category("[4] Satiety"), Description("饱食度上限"), DisplayName("[1] SatietyMax")]
+    [Category("[3] Satiety"), Description("饱食度上限"), DisplayName("[1] SatietyMax")]
     public double SatietyMax { get; set; }
-    [Category("[4] Satiety"), Description("饱食度变化量"), DisplayName("[2] BasicSatietyDecrease")]
+    [Category("[3] Satiety"), Description("饱食度变化量"), DisplayName("[2] BasicSatietyDecrease")]
     public double BasicSatietyDecrease { get; set; }
+    [Category("[3] San"), Description("标准化Sat低标准 : ([0, 1])"), DisplayName("[3] NormalizedLowSatietyBar")]
+    public double NormalizedLowSatietyBar { get; set; }
 
-    [Category("[4] Satiety"), Description("饱食度变化倍率"), DisplayName("[3] SanietyMultiplier")]
-    public double SanietyMultiplier { get; set; }
 
     //San
-    [Category("[5] San"), Description("San值"), DisplayName("[0] Sanity")]
+    [Category("[4] San"), Description("San值"), DisplayName("[0] Sanity")]
     public double Sanity { get; set; }
-    [Category("[5] San"), Description("San值上限"), DisplayName("[1] SanityMax")]
+    [Category("[4] San"), Description("San值上限"), DisplayName("[1] SanityMax")]
     public double SanityMax { get; set; }
-    [Category("[5] San"), Description("San值变化量"), DisplayName("[2] BasicSanityDecrase")]
+    [Category("[4] San"), Description("San值变化量"), DisplayName("[2] BasicSanityDecrase")]
     public double BasicSanityDecrase { get; set; }
+    [Category("[4] San"), Description("标准化San低标准 : ([0, 1])"), DisplayName("[3] NormalizedLowSanityBar")]
+    public double NormalizedLowSanBar { get; set; }
 
-    [Category("[5] San"), Description("San值变化倍率"), DisplayName("[3] SanityMultiplier")]
-    public double SanityMultiplier { get; set; }
 
-    [Category("[3] Health"), Description("存活")]
-    public bool IsAlive { get; set; } = true;
-    [Category("[6] Type"), Description("菜品属性倍率")]
+
+    [Category("[5] Type"), Description("员工生物类型(used)")]
+    public WorkerType Type { get; set; }
+    [Category("[5] Type"), Description("菜品属性倍率")]
     public List<WorkerFoodFavor> FoodPopertyMultiplier { get; set; } = new List<WorkerFoodFavor>();
 
-    [Category("[8] LineBank"), Description("正常数值员工对话")]
-    private List<string> NormalLineBank { get; set; } = new();
-    [Category("[8] LineBank"), Description("濒死员工对话")]
-    private List<string> DyingLineBank { get; set; } = new();
-    [Category("[8] LineBank"), Description("良好员工对话")]
-    private List<string> GoodLineBank { get; set; } = new();
-
-    [Category("[7] Contest"), Description("员工是否参与暴乱")]
-    public bool IsParticatedInContest { get; set; }
+    [Category("[6] Revolt"), Description("员工是否反抗")]
+    public bool WillRevolt { get; set; }
 
 
+    [Category("[7] Lines"), Description("员工标准对话 - 初次见面"), DisplayName("[0] HelloLines")]
+    public List<string> HelloLines { get; set; } = new List<string>();
+
+    [Category("[7] Lines"), Description("员工标准对话 - San正常"), DisplayName("[1] StandardLines_NormalSAN")]
+    public List<string> StandardLines_NormalSAN { get; set; } = new List<string>();
+    [Category("[7] Lines"), Description("员工标准对话 - San低下"), DisplayName("[2] StandardLines_LowlSAN")]
+    public List<string> StandardLines_LowlSAN { get; set; } = new();
 
 
-    private List<string>[] LineBanks;
+    [Category("[7] Lines"), Description("员工低健康度对话 - San正常"), DisplayName("[3] LowHealthLines_NormalSAN")]
+    public List<string> LowHealthLines_NormalSAN { get; set; } = new();
+    [Category("[7] Lines"), Description("员工低健康度对话 - San低下"), DisplayName("[4] LowHealthLines_LowSAN")]
+    public List<string> LowHealthLines_LowSAN { get; set; } = new();
 
-    public Worker()
+    [Category("[7] Lines"), Description("员工低饱食度对话 - San正常"), DisplayName("[5] LowSatietyLines_NormalSAN")]
+    public List<string> LowSatietyLines_NormalSAN { get; set; } = new();
+    [Category("[7] Lines"), Description("员工低饱食度对话 - San低下"), DisplayName("[6] LowSatietyLines_LowSAN")]
+    public List<string> LowSatietyLines_LowSAN { get; set; } = new();
+
+    [Category("[8] Lines"), Description("员工0San对话 "), DisplayName("[7] ZeroSatietyLines")]
+    public List<string> ZeroSatietyLines { get; set; } = new();
+
+    [Category("[8] Lines"), Description("员工暴乱对话 - San正常"), DisplayName("[7] RevoltLine_NormalSAN")]
+    public List<string> RevoltLine_NormalSAN { get; set; } = new();
+    [Category("[8] Lines"), Description("员工0San对话 - San低下"), DisplayName("[7] RevoltLine_LowSAN")]
+    public List<string> RevoltLine_LowSAN { get; set; } = new();
+
+
+
+
+    [Category("[8] Events"), Description("员工San归零事件")]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public GameEventManager.GameEvent OnSanIsZero { get; set; } = new();
+    [Category("[8] Events"), Description("员工死亡事件")]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public GameEventManager.GameEvent OnDead { get; set; } = new();
+
+    public double RivoltPoint
     {
-      LineBanks = new List<string>[3];
-      LineBanks[(int)WorkerStatus.Good] = GoodLineBank;
-      LineBanks[(int)WorkerStatus.Normal] = NormalLineBank;
-      LineBanks[(int)WorkerStatus.Dying] = DyingLineBank;
+      get
+      {
+        double R = 0.5 * NormalizedHealth * (2 * (0.4 - NormalizedSatiety) + 8 * (0.4 - NormalizedSanity));
+        return WillRevolt ? 1.5 * R : R;
+      }
     }
+
+    private bool Met = false;
+
+
+
+    public double NormalizedHealth { get { return Health / HealthMax; } }
+    public double NormalizedSatiety { get { return Satiety / SatietyMax; } }
+    public double NormalizedSanity { get { return Sanity / SanityMax; } }
+
+    private List<GameEventManager.GameEvent> TriggeredGameEvents = new() { };
+
+    //All Sentence should be displayed
+    public List<string> LineBank = new List<string>();
+
     public void Eat(Dish dish)
     {
       double mul = 0.0;
@@ -108,13 +152,122 @@ namespace Assets.BasicModule.Model
         }
       }
 
-      Sanity += dish.DishSanity * dish.Successfulness * SanityMultiplier * mul;
-      Health += dish.DishHealth * dish.Successfulness * HealthMultiplier * mul;
-      Satiety += dish.DishSatiety * dish.Successfulness * SanietyMultiplier;
+      Sanity += dish.DishSanity * mul;
+      Health += dish.DishHealth * mul;
+      Satiety += dish.DishSatiety;
+    }
+    public void LoadBank()
+    {
+      LineBank.Clear();
+
+      if (Sanity == 0)
+      {
+        LineBank.Add(ZeroSatietyLines[GameManager.Instance.RNG.Next(ZeroSatietyLines.Count)]);
+        return;
+      }
+
+      //first met
+      if (!Met)
+      {
+        LineBank.Add(HelloLines[GameManager.Instance.RNG.Next(HelloLines.Count)]);
+        Met = true;
+      }
+
+      //Dying
+      if (NormalizedHealth < NormalizedLowHealthBar)
+      {
+        if (NormalizedSanity < NormalizedLowSanBar)
+        {
+          LineBank.Add(LowHealthLines_LowSAN[GameManager.Instance.RNG.Next(LowHealthLines_LowSAN.Count)]);
+        }
+        else
+        {
+          LineBank.Add(LowHealthLines_NormalSAN[GameManager.Instance.RNG.Next(LowHealthLines_NormalSAN.Count)]);
+        }
+        return;
+      }
+
+      //low sat
+      if (NormalizedSatiety < NormalizedLowSatietyBar)
+      {
+        if (NormalizedSanity < NormalizedLowSanBar)
+        {
+          LineBank.Add(LowSatietyLines_LowSAN[GameManager.Instance.RNG.Next(LowSatietyLines_LowSAN.Count)]);
+        }
+        else
+        {
+          LineBank.Add(LowSatietyLines_NormalSAN[GameManager.Instance.RNG.Next(LowSatietyLines_NormalSAN.Count)]);
+        }
+      }
+
+      //event
+      GameEventManager.GameEvent CurrentMostEvent = new();
+      foreach (GameEventManager.GameEvent Event in GameEventManager.Instance.Events)
+      {
+        if (!TriggeredGameEvents.Contains(Event))
+        {
+          CurrentMostEvent = Event;
+          break;
+        }
+      }
+
+      if (CurrentMostEvent.ID > 500)//is presist or not
+      {
+        TriggeredGameEvents.Add(CurrentMostEvent);
+      }
+
+      EventWorkerLine ewl = GameManager.Instance.EventWorkerLines.Find(x => x.WorkerID == ID && x.EventID == CurrentMostEvent.ID);
+      if (ewl != null)
+      {
+        if (NormalizedSanity < NormalizedLowSanBar)
+        {
+          LineBank.Add(ewl.Lines_LowSan[GameManager.Instance.RNG.Next(ewl.Lines_LowSan.Count)]);
+        }
+        else
+        {
+          LineBank.Add(ewl.Lines_NormalSan[GameManager.Instance.RNG.Next(ewl.Lines_NormalSan.Count)]);
+        }
+
+      }
+      else if (Met)
+      {
+        if (NormalizedSanity < NormalizedLowSanBar)
+        {
+          LineBank.Add(StandardLines_LowlSAN[GameManager.Instance.RNG.Next(StandardLines_LowlSAN.Count)]);
+        }
+        else
+        {
+          LineBank.Add(StandardLines_NormalSAN[GameManager.Instance.RNG.Next(StandardLines_NormalSAN.Count)]);
+        }
+      }
+
+      //On Revolt
+      if(GameManager.Instance.IsOnRevolt)
+      {
+        if(NormalizedSanity < NormalizedLowSanBar)
+        {
+          LineBank.Add(RevoltLine_LowSAN[GameManager.Instance.RNG.Next(RevoltLine_LowSAN.Count)]);
+        }
+        else
+        {
+          LineBank.Add(RevoltLine_NormalSAN[GameManager.Instance.RNG.Next(RevoltLine_NormalSAN.Count)]);
+        }
+      }
+
+
+
+
+
+
+
     }
     public void UpdateStatus()
     {
-      if()
+
+      double HealthAdjustment = (NormalizedSatiety - 0.3) * 40 + (NormalizedSanity - 0.3) * 20;
+      Health = HealthAdjustment + Health > HealthMax ? HealthMax : HealthAdjustment + Health;
+
     }
+
   }
 }
