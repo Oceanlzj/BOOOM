19afdf52d0546b9e501cf0ae290509c9b1da507b
Oceanlzj <oceanlzj@outlook.com>
2024-04-29 03:52:42 +0800

    Process QTE Cooking Finish

--- Assets/Scripts/ProcessScene/ProcessSceneManager.cs
+++ Assets/Scripts/ProcessScene/ProcessSceneManager.cs
@@ -52,14 +52,13 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
   public int PipeNum1;
   public int PipeNum2;
 
-  public List<IngredientItem> _dishesList;
+  public List<IngredientItem> IngerdientItemList;
   public int CurrentIndex = 0;
 
 
   public float seconds;       // 创建时间间隔
 
 
-  private ManagerStutes _stutes;
 
   void Start()
   {
@@ -72,8 +71,7 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
     }
 
 
-    _dishesList = new List<IngredientItem>();
-    _stutes = ManagerStutes.Creating;
+    IngerdientItemList = new List<IngredientItem>();
     StartNewDay();
     DoorAnimator.Play("DoorOpen");
   }
@@ -81,11 +79,23 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
   // Update is called once per frame
   void Update()
   {
-    if (GoNext)
+    if (qteManager.Status == QteStatus.FinishCutting)
     {
-      float alphaChange = 0.5f * Time.deltaTime;
-
+      print(qteManager.GetCutVectorNum());
+      qteManager.StartCookGame();
+      return;
     }
+
+    if (qteManager.Status == QteStatus.FinishCooking)
+    {
+      //qteManager.FinishCookGame();
+      DishOut();
+      print(qteManager.GetCookTotalTime());
+      DoorAnimator.Play("DoorOpen");
+      qteManager.Status = QteStatus.WaitingForCompress;
+      return;
+    }
+
   }
 
   private void StartNewDay()
@@ -97,7 +107,6 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
       curNum++;
     }
 
-    _stutes = ManagerStutes.Waiting;
   }
   public void UpdateText()
   {
@@ -115,7 +124,7 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
       PipeAnimator[number].Play("PipeOut");
       IngredientItem dish = Instantiate(dishProfab, Pipes[number].GetChild(0).position /*+ new Vector3(0, 0.5f, 0)*/, Quaternion.identity);
       dish.Ingredient = GameManager.Instance.Ingredients[CurrentIndex];
-      _dishesList.Add(dish);
+      IngerdientItemList.Add(dish);
       dish.SetPos(Pipes[number].GetChild(1).position + new Vector3(0, 1, 0));
       dish.PipeNum = number;
 
@@ -138,10 +147,30 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
 
   public void RemoveIngerdient(IngredientItem dish)
   {
-    _dishesList.Remove(dish);
-    _stutes = ManagerStutes.Destroy;
+    IngerdientItemList.Remove(dish);
   }
 
+  public void DishOut()
+  {
+
+    Dish dish = DataFactory.Instance().GetDishByRecipe(IngredientsOnMachine[0].ID, IngredientsOnMachine[1].ID);
+
+    Destroy(IngsOnMachine[1].gameObject);
+    Destroy(IngsOnMachine[0].gameObject);
+
+    //cook Dish
+    cd = Instantiate(CookedDish, CookedDishPos);
+    cd.Dish = dish;
+    IngredientsOnMachine.Clear();
+    Machine.enabled = false;
+
+    //new 
+
+    Portion = (int)((qteManager.GetCutVectorNum() * qteManager.GetCookTotalTime()) + 1);
+    TextArea.text = dish.ToString() + " x " + Portion;
+    TextArea.text += "\n\n确定压缩？";
+
+  }
 
   public void OnYesClicked()
   {
@@ -152,50 +181,31 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
       return;
     }
 
-    if (Machine.enabled)
+    if (Machine.enabled && qteManager.Status == QteStatus.Waiting)
     {
       //cook ani
-      //cook destory
-      if (IngsOnMachine.Count < 2) { return; }
       PipeNum1 = IngsOnMachine[0].PipeNum;
       PipeNum2 = IngsOnMachine[1].PipeNum;
-      try
-      {
-        Dish dish = DataFactory.Instance().GetDishByRecipe(IngredientsOnMachine[0].ID, IngredientsOnMachine[1].ID);
-        Portion = Random.Range(1, 3);
+      RemoveIngerdient(IngsOnMachine[0]);
+      RemoveIngerdient(IngsOnMachine[1]);
+      CreateIngredient(PipeNum1);
+      CreateIngredient(PipeNum2);
 
-        Destroy(IngsOnMachine[1].gameObject);
-        Destroy(IngsOnMachine[0].gameObject);
 
-        //cook Dish
-        cd = Instantiate(CookedDish, CookedDishPos);
-        cd.Dish = dish;
-        IngredientsOnMachine.Clear();
-        Machine.enabled = false;
+      //cook destory
+      if (IngsOnMachine.Count < 2) { return; }
+      IngsOnMachine[0]._status = DishStatus.Cooking;
+      IngsOnMachine[1]._status = DishStatus.Cooking;
 
-        //new 
-        CreateIngredient(PipeNum1);
-        CreateIngredient(PipeNum2);
+      DoorAnimator.Play("DoorClose");
 
-        TextArea.text = "确定压缩？";
+      qteManager.StartCutGame();
 
-        QteManager.Instance.StartQteGame();
-      }
-      catch
-      {
-        StopText = true;
-        TextArea.text = "无效的配方，请重试！";
-        if (_dishesList.Count <= 3)
-        {
 
-          TextArea.text = "没有更多可料理的了……\n\n 去服务窗口吧";
-          GoNext = true;
-        }
-        OnNoClicked();
-      }
     }
-    else
+    else if (qteManager.Status == QteStatus.WaitingForCompress)
     {
+      
       if (GameManager.Instance.DishesInventory.ContainsKey(cd.Dish))
       {
         GameManager.Instance.DishesInventory[cd.Dish] += Portion;
@@ -204,8 +214,12 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
       {
         GameManager.Instance.DishesInventory.Add(cd.Dish, Portion);
       }
+
+
+      qteManager.Status = QteStatus.Waiting;
+      TextArea.text = "";
       Destroy(cd.gameObject);
-      if (NoMoreIngredient && _dishesList.Count < 2)
+      if (NoMoreIngredient && IngerdientItemList.Count < 2)
       {
         TextArea.text = "没有更多食材了……\n\n 去服务窗口吧";
         GoNext = true;
@@ -214,6 +228,7 @@ public class ProcessSceneManager : Singleton<ProcessSceneManager>
       {
         Machine.enabled = true;
       }
+
     }
 
 
--- Assets/Scripts/QteGame/CutQte.cs
+++ Assets/Scripts/QteGame/CutQte.cs
@@ -4,75 +4,75 @@ using UnityEngine;
 
 public class CutQte : MonoBehaviour
 {
-    public Transform poiter;            // 滑块
-    public Transform area;              // 识别区
-    public int maxGameNum;
-    
-    private int _vectorNum;
-    private int _gameNum;
+  public Transform poiter;            // 滑块
+  public Transform area;              // 识别区
+  public int maxGameNum;
 
-    private Pointer _pointer;
-    // Start is called before the first frame update
-    void Start()
+  private int _vectorNum;
+  private int _gameNum;
+
+  private Pointer _pointer;
+  // Start is called before the first frame update
+  void Start()
+  {
+    _vectorNum = 0;
+    _gameNum = 0;
+    _pointer = poiter.GetComponent<Pointer>();
+    area.gameObject.SetActive(false);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //InitGame();
+    //print(area.transform.rotation.z);
+  }
+
+  public void StartGame()
+  {
+    _vectorNum = 0;
+    _gameNum = 0;
+    _pointer.StartGame();
+    area.gameObject.SetActive(true);
+    InitGame();
+  }
+
+  public void StopGame()
+  {
+    //_vectorNum = 0;
+    //_gameNum = 0;
+    area.gameObject.SetActive(false);
+    _pointer.StopGame();
+  }
+
+  public int GetVectorNum()
+  {
+    return _vectorNum;
+  }
+
+  public void InitGame()
+  {
+    //30~-155
+    area.transform.rotation = Quaternion.Euler(0, 0, Random.Range(-155f, 30f));
+    _pointer.Reset();
+  }
+
+  public void CheckVector(bool vector)
+  {
+    _gameNum++;
+    if (vector)
     {
-        _vectorNum = 0;
-        _gameNum = 0;
-        _pointer = poiter.GetComponent<Pointer>();
-        area.gameObject.SetActive(false);
+      _vectorNum++;
     }
 
-    // Update is called once per frame
-    void Update()
+    if (_gameNum >= maxGameNum)
     {
-        //InitGame();
-        //print(area.transform.rotation.z);
+      QteManager.Instance.FinishCutGame();
+      StopGame();
     }
-
-    public void StartGame()
+    else
     {
-        _vectorNum = 0;
-        _gameNum = 0;
-        _pointer.StartGame();
-        area.gameObject.SetActive(true);
-        InitGame();
+      InitGame();
     }
-    
-    public void StopGame()
-    {
-        //_vectorNum = 0;
-        //_gameNum = 0;
-        area.gameObject.SetActive(false);
-        _pointer.StopGame();
-    }
-
-    public int GetVectorNum()
-    {
-        return _vectorNum;
-    }
-
-    public void InitGame()
-    {
-        //30~-155
-        area.transform.rotation = Quaternion.Euler(0, 0, Random.Range(-155f,30f));
-        _pointer.Reset();
-    }
-
-    public void CheckVector(bool vector)
-    {
-        _gameNum++;
-        if (vector)
-        {
-            _vectorNum++;
-        }
-
-        if (_gameNum >= maxGameNum)
-        {
-            QteManager.Instance.FinishCutGame();
-            StopGame();
-        }
-        else
-        {
-            InitGame();
-        }
-    }
+  }
 }
